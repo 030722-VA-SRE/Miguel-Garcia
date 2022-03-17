76e7fe3a5d3acffd3eec96ba937825a70052a475
Garcike <miggarcia338@gmail.com>
2022-03-16 22:10:21 -0500

    cleaning up file

--- project0/src/main/java/com/revature/util/ConnectionUtil.java
+++ project0/src/main/java/com/revature/util/ConnectionUtil.java
@@ -10,16 +10,7 @@ public class ConnectionUtil {
 	private static Connection con;
 	
 	public static Connection getConnectionFromEnv() throws SQLException {
-		/*-
-		 * search for Edit the System environment variables
-		 * 	- click on environment variables
-		 * 	- under System variables, click new
-		 * 	- enter a variable name, and its value
-		 * 		- ie: DB_URL, [url_value]
-		 * 	- press ok
-		 * 
-		 * NOTE: YOU MIGHT HAVE TO RESTART STS FOR YOUR NEW ENVIRONMENT VARIABLES TO LOAD!
-		 */
+
 		String url = System.getenv("DB_URL");
 		String username = System.getenv("DB_USER");
 		String password = System.getenv("DB_PASS");
