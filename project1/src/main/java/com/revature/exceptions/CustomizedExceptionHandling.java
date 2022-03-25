188ef91c5d1b8bffa99647572994b01f6281e1e6
Garcike <miggarcia338@gmail.com>
2022-03-24 22:03:54 -0500

    adding exceptions

---
+++ project1/src/main/java/com/revature/exceptions/CustomizedExceptionHandling.java
@@ -0,0 +1,36 @@
+package com.revature.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/*
+ * ResponseEntityExceptionHandler is a convenient base class that wishes to provide
+ * centralized exception handling across all @RequestMapping methods through @ExceptionHandler methods.
+ */
+//@Controller advice allows you to handle exceptions across the whole application
+@ControllerAdvice
+public class CustomizedExceptionHandling extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception, WebRequest webRequest) {
+		ExceptionResponse response = new ExceptionResponse();
+		response.setMessage("User not found");
+		response.setDateTime(LocalDateTime.now());
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}//end 
+	
+	@ExceptionHandler(UserAlreadyExistException.class)
+	public ResponseEntity<Object> handleUserAlreadyExistException(UserAlreadyExistException exception, WebRequest webRequest){
+		ExceptionResponse response = new ExceptionResponse();
+		response.setMessage("User already exist");
+		response.setDateTime(LocalDateTime.now());
+		return new ResponseEntity<>(response, HttpStatus.UNPROCESSABLE_ENTITY);
+	}
+	
+}//end
